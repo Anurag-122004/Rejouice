39fc275ee8733e9898278c01050e7940663129f7
Anurag Kaushik <anurag11b12345@gmail.com>
2024-03-20 18:59:50 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+
+
+function lenisAnimation() {
+    let tl = gsap.timeline() 
+
+    tl.to(".animated-element", {
+        
+    })
+
+    //lenis----------------------
+    const lenis = new Lenis()
+
+    lenis.on('scroll', (e) => {
+    console.log(e)
+    })
+
+    function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+}
+
+lenisAnimation();
 function mouseEffect() {
     let page1Content = document.querySelector("#page1-content");
-let cursor = document.querySelector("#cursor");
+    let cursor = document.querySelector("#cursor");
 
 page1Content.addEventListener("mousemove", (dets) => {
     gsap.to(cursor, {
@@ -25,3 +50,21 @@ page1Content.addEventListener("mouseleave", () => {
 }
 
 mouseEffect();
+
+function page2Animation() {
+    gsap.from(".elem #shifted-h1", {
+        y: -120,
+        stagger: 0.25,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#page2",
+            scroller: "#main",
+            start: "top 80%", // Adjusted start value
+            end: "top 53%", // Adjusted end value
+            markers: true, // new comment for markers
+            scrub: 2
+        }
+    });
+}
+
+page2Animation();
